2db19a32e8fc4ca95fc591d9b6a55ae8d65593a0
Racksoup <39200478+Racksoup@users.noreply.github.com>
2019-01-14 17:39:41 -0500

    Updates

---
+++ precache-manifest.71692aac0572b0ccdbf407e293457a57.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7096105dc4f6d5a29d1",
+    "url": "/social-card/static/js/runtime~main.c7096105.js"
+  },
+  {
+    "revision": "fb9a449c66bc0aff34b5",
+    "url": "/social-card/static/js/main.fb9a449c.chunk.js"
+  },
+  {
+    "revision": "adf7cbfdfa2118501191",
+    "url": "/social-card/static/js/1.adf7cbfd.chunk.js"
+  },
+  {
+    "revision": "2f9cca1408e82c16e1acf5ab95dcab37",
+    "url": "/social-card/index.html"
+  }
+];
